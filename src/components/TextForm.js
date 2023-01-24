8bde5a647b6db0dcbd685df555609e245a109bff
chahat <chahatba9876@gmail.com>
2023-01-24 16:20:37 +0530

    comment

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -49,8 +49,8 @@ const copyText=()=>{
     </div>
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
       <h2>Text summary</h2>
-      <p>{text.split(" ").length} words {text.length} characters</p>
-      <p>{0.008*text.split(" ").length} minutes read</p>
+      <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words {text.length} characters</p>
+      <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
       <h2>Preview</h2>
       <p>{text}</p>
     </div>
